d5f20a393e06f3916923ce0f5e0c93dc507e1262
Yosuke MIYAJIMA <mayo.suke@gmail.com>
2017-01-18 19:46:16 +0900

    Update webtest.js

--- webtest.js
+++ webtest.js
@@ -2,10 +2,12 @@ var pageVariations = [
 function() {
   console.log('original');
   document.getElementsByTagName('p')[0].innerHTML='hello web test!!! this is original';
+  console.log('hoge');
 },
 function() {
   console.log('pattern1');
   document.getElementsByTagName('p')[0].innerHTML='hello web test!!! this is pattern1';
+  console.log('fuga');
 }];
 $(document).ready(
   pageVariations[chosenVariation]
